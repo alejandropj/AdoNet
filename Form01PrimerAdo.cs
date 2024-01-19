388bc952a251ead14b3900fc56dff32110a15e2f
alejandropj <alejandroparrajimenez@gmail.com>
2024-01-19 11:17:20 +0100

    19/01

--- Form01PrimerAdo.cs
+++ Form01PrimerAdo.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Data.SqlClient;
 
 namespace AdoNet
@@ -18,18 +19,36 @@ namespace AdoNet
             this.cn = new SqlConnection(this.connectionString);
             //Vamos a recuperar un evento de la conexion para que nos
             //indique su cambio de estado
-
+            this.cn.StateChange += Cn_StateChange;
             this.com = new SqlCommand();
 
         }
+
+        private void Cn_StateChange(object sender, StateChangeEventArgs e)
+        {
+            this.lblMensaje.Text = "La conexión está pasando de " + e.OriginalState +
+                " a " + e.CurrentState;
+        }
+
         private void btnConectar_Click(object sender, EventArgs e)
         {
             //this.cn.ConnectionString = this.connectionString;
-            if (this.cn.State == System.Data.ConnectionState.Closed)
+            //Podemos capturar la excepción con try catch
+            try
             {
-                this.cn.Open();
+                if (this.cn.State == System.Data.ConnectionState.Closed)
+                {
+                    this.cn.Open();
+                }
+                this.lblMensaje.BackColor = Color.LightGreen;
+
             }
-            this.lblMensaje.BackColor = Color.LightGreen;
+            catch(SqlException ex)
+            {
+                this.lblMensaje.Text = "Excepción SQL. " + ex.ToString();
+                    this.lblMensaje.BackColor = Color.Red;
+            }
+
         }
         private void btnDesconnect_Click(object sender, EventArgs e)
         {
@@ -39,7 +58,25 @@ namespace AdoNet
 
         private void btnRead_Click(object sender, EventArgs e)
         {
+            this.com.Connection = this.cn;
+            string sql = "select * from EMP";
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.reader = this.com.ExecuteReader();
+            for (int i = 0; i < this.reader.FieldCount; i++)
+            {
+                string columna = this.reader.GetName(i);
+                string tipoDato = this.reader.GetDataTypeName(i);
+                this.lstColumnas.Items.Add(columna);
+                this.lstTiposDato.Items.Add(tipoDato);
+            }
 
+            while (this.reader.Read())
+            {
+                string apellido = this.reader["APELLIDO"].ToString();
+                this.lstApellidos.Items.Add(apellido);
+            }
+            this.reader.Close();
         }
     }
 }
